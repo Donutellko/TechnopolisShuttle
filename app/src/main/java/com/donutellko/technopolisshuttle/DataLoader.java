77977726fa792b4ee613be301d00482ecddc7e29
Donutellko <donutellko@gmail.com>
2017-07-08 01:47:45 +0300

    Great GUI, weekday spinner, direction choosed by tumblers

--- app/src/main/java/com/donutellko/technopolisshuttle/DataLoader.java
+++ app/src/main/java/com/donutellko/technopolisshuttle/DataLoader.java
@@ -2,10 +2,11 @@ package com.donutellko.technopolisshuttle;
 
 import android.util.Log;
 
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import com.donutellko.technopolisshuttle.DataLoader.STime;
 
 /**
  * Created by Donut on 04.07.2017.
@@ -14,17 +15,21 @@ import java.util.List;
 // Получает данные из памяти и базы данных
 public class DataLoader {
 
-	public class STime {
+	public static class STime {
 		int hour, min;
 
 		public STime (int hour, int min) {
 			this.hour = hour;
 			this.min = min;
 		}
+
+		@Deprecated
 		public STime (Date d) {
 			hour = d.getHours();
 			min = d.getMinutes();
 		}
+
+		@Deprecated
 		public STime (java.sql.Time t) {
 			hour = t.getHours();
 			min = t.getMinutes();
@@ -32,8 +37,7 @@ public class DataLoader {
 
 		public boolean isBefore(STime t) {
 			if (hour < t.hour) return true;
-			if (hour == t.hour && min < t.min)
-				return true;
+			if (hour == t.hour && min < t.min) return true;
 			return false;
 		}
 
@@ -53,111 +57,23 @@ public class DataLoader {
 				if (this.isBefore(t)) list.add(t);
 			return list;
 		}
+
+		public int compareTo(STime t) {
+			if (hour < t.hour)
+				return -1;
+			if (hour == t.hour && min == t.min)
+				return 0;
+			return 1;
+		}
+
+		public String toString() {
+			return hour + ":" + min;
+		}
+
+		public boolean isZero() {
+			return hour == 0 && min == 0;
+		}
 	}
-
-	@SuppressWarnings("deprecation")
-	Time[] defaultTimeFrom = new Time[]{
-			new Time(7, 45, 0),
-
-			new Time(8, 00, 0),
-			new Time(8, 10, 0),
-			new Time(8, 20, 0),
-			new Time(8, 30, 0),
-			new Time(8, 35, 0),
-			new Time(8, 40, 0),
-			new Time(8, 50, 0),
-
-			new Time(9, 00, 0),
-			new Time(9, 10, 0),
-			new Time(9, 15, 0),
-			new Time(9, 20, 0),
-			new Time(9, 30, 0),
-			new Time(9, 40, 0),
-			new Time(9, 50, 0),
-			new Time(9, 55, 0),
-
-			new Time(10, 00, 0),
-			new Time(10, 10, 0),
-			new Time(10, 20, 0),
-			new Time(10, 30, 0),
-			new Time(10, 35, 0),
-			new Time(10, 40, 0),
-			new Time(10, 50, 0),
-
-			new Time(11, 00, 0),
-			new Time(11, 10, 0),
-			new Time(11, 20, 0),
-			new Time(11, 30, 0),
-			new Time(11, 50, 0),
-
-			new Time(12, 10, 0),
-			new Time(12, 30, 0),
-
-			new Time(13, 10, 0),
-			new Time(13, 50, 0),
-
-			new Time(14, 30, 0),
-
-			new Time(15, 10, 0),
-			new Time(15, 30, 0),
-
-			new Time(16, 10, 0),
-			new Time(16, 50, 0),
-
-			new Time(17, 20, 0),
-	};
-
-	@SuppressWarnings("deprecation")
-	Time[] defaultTimeTo = new Time[]{
-			new Time(9, 30, 0),
-
-			new Time(10, 10, 0),
-			new Time(10, 50, 0),
-
-			new Time(11, 30, 0),
-
-			new Time(12, 10, 0),
-			new Time(12, 50, 0),
-
-			new Time(13, 30, 0),
-
-			new Time(14, 10, 0),
-			new Time(14, 50, 0),
-
-			new Time(15, 10, 0),
-			new Time(15, 30, 0),
-			new Time(15, 50, 0),
-
-			new Time(16, 00, 0),
-			new Time(16, 30, 0),
-			new Time(16, 50, 0),
-
-			new Time(17, 00, 0),
-			new Time(17, 10, 0),
-			new Time(17, 30, 0),
-			new Time(17, 40, 0),
-			new Time(17, 50, 0),
-
-			new Time(18, 00, 0),
-			new Time(18, 10, 0),
-			new Time(18, 20, 0),
-			new Time(18, 30, 0),
-			new Time(18, 40, 0),
-			new Time(18, 50, 0),
-
-			new Time(19, 10, 0),
-			new Time(19, 20, 0),
-			new Time(19, 30, 0),
-			new Time(19, 40, 0),
-			new Time(19, 50, 0),
-
-			new Time(20, 10, 0),
-			new Time(20, 45, 0),
-
-			new Time(21, 20, 0),
-
-	};
-
 
 	public TimeTable getFullDefaultInfo() {
 		return new TimeTable(defaultTimeFrom, defaultTimeTo);
@@ -172,13 +88,114 @@ public class DataLoader {
 		//TODO
 		return getFullDefaultInfo();
 	}
+
+	STime[] defaultTimeFrom = new STime[] {
+			new STime(7, 45),
+
+			new STime(8, 00),
+			new STime(8, 10),
+			new STime(8, 20),
+			new STime(8, 30),
+			new STime(8, 35),
+			new STime(8, 40),
+			new STime(8, 50),
+
+			new STime(9, 00),
+			new STime(9, 10),
+			new STime(9, 15),
+			new STime(9, 20),
+			new STime(9, 30),
+			new STime(9, 40),
+			new STime(9, 50),
+			new STime(9, 55),
+
+			new STime(10, 00),
+			new STime(10, 10),
+			new STime(10, 20),
+			new STime(10, 30),
+			new STime(10, 35),
+			new STime(10, 40),
+			new STime(10, 50),
+
+			new STime(11, 00),
+			new STime(11, 10),
+			new STime(11, 20),
+			new STime(11, 30),
+			new STime(11, 50),
+
+			new STime(12, 10),
+			new STime(12, 30),
+
+			new STime(13, 10),
+			new STime(13, 50),
+
+			new STime(14, 30),
+
+			new STime(15, 10),
+			new STime(15, 30),
+
+			new STime(16, 10),
+			new STime(16, 50),
+
+			new STime(17, 20),
+	};
+
+	STime[] defaultTimeTo = new STime[]{
+			new STime(9, 30),
+
+			new STime(10, 10),
+			new STime(10, 50),
+
+			new STime(11, 30),
+
+			new STime(12, 10),
+			new STime(12, 50),
+
+			new STime(13, 30),
+
+			new STime(14, 10),
+			new STime(14, 50),
+
+			new STime(15, 10),
+			new STime(15, 30),
+			new STime(15, 50),
+
+			new STime(16, 00),
+			new STime(16, 30),
+			new STime(16, 50),
+
+			new STime(17, 00),
+			new STime(17, 10),
+			new STime(17, 30),
+			new STime(17, 40),
+			new STime(17, 50),
+
+			new STime(18, 00),
+			new STime(18, 10),
+			new STime(18, 20),
+			new STime(18, 30),
+			new STime(18, 40),
+			new STime(18, 50),
+
+			new STime(19, 10),
+			new STime(19, 20),
+			new STime(19, 30),
+			new STime(19, 40),
+			new STime(19, 50),
+
+			new STime(20, 10),
+			new STime(20, 45),
+
+			new STime(21, 20),
+
+	};
 }
 
 class TimeTable {
 	public List<Line> lines;
-	public Time[] from, to;
+	public STime[] from, to;
 
-	public TimeTable(Time[] from, Time[] to) {
+	public TimeTable(STime[] from, STime[] to) {
 		lines = new ArrayList<Line>();
 		this.from = from;
 		this.to = to;
@@ -216,6 +233,18 @@ class TimeTable {
 		Log.i("lol", this.toString());
 	}
 
+	public List<STime> getTimeAfter (STime now, boolean To) {
+		Log.i("lolaofaosdjfsdfoajsf", to.length + " " + from.length + " " + To);
+		STime[] cur = To ? to : from;
+		List<STime> result = new ArrayList<>();
+
+		for (STime t : cur)
+			if (now.isBefore(t))
+				result.add(t);
+
+		return result;
+	}
+
 	public TimeTable(Line[] lines) {
 		this.lines = new ArrayList<Line>(lines.length);
 		for (Line l : lines)
@@ -231,27 +260,18 @@ class TimeTable {
 	}
 
 	class Line {
-		public Time time;
+		public STime time;
 		public boolean from, to;
 
-		public Line(Time time, boolean from, boolean to) {
+		public Line(STime time, boolean from, boolean to) {
 			this.time = time;
 			this.from = from;
 			this.to = to;
 		}
 
-		public boolean isBefore(Time current) {
-			int curH = current.getHours(), schedH = time.getHours(),
-					curM = current.getMinutes(), schedM = time.getMinutes();
-
-			if (curH > schedH) return true;
-			else if (curH < schedH) return false;
-			return curM > schedM;
-		}
-
-		public boolean isBefore(Date current) {
-			int curH = current.getHours(), schedH = time.getHours(),
-					curM = current.getMinutes(), schedM = time.getMinutes();
+		public boolean isBefore(STime current) {
+			int curH = current.hour, schedH = time.hour,
+					curM = current.min, schedM = time.min;
 
 			if (curH > schedH) return true;
 			else if (curH < schedH) return false;
--- app/src/main/java/com/donutellko/technopolisshuttle/MainActivity.java
+++ app/src/main/java/com/donutellko/technopolisshuttle/MainActivity.java
@@ -10,19 +10,22 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -33,32 +36,34 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
+import com.donutellko.technopolisshuttle.DataLoader.STime;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+	//CONSTANTS
 	private static final int COUNT_TO_SHOW_ON_SHORT = 5;
 	private static final boolean USE_THREE_COLUMNS_FULL_SCHEDULE = false;
-	private boolean toTechnopolis = true;
+
+	// CheckBox values
 	private boolean showPast = true;
+
 	private DataLoader dataLoader = new DataLoader();
 	private TimeTable timeTable;
 	private LayoutInflater layoutInflater;
-	LinearLayout contentView; // Область контента (всё кроме нижней панели)
-	private View shortView, fullView, mapView;
-	Calendar curtime = Calendar.getInstance();
+	Calendar curtime;
 
-	int width, height;
+	// Подставляемые View
+	private View shortView, fullView, mapView;
+	LinearLayout contentView; // Область контента (всё кроме нав. панели)
+	ToggleButton toggleButtonToTechnopolis, toggleButtonToUnderground;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		Display display = getWindowManager().getDefaultDisplay();
-		width = display.getWidth();  // deprecated
-		height = display.getHeight();  // deprecated
 
 		layoutInflater = getLayoutInflater();
 
@@ -68,50 +73,89 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 		BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 		navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 		navigation.setSelectedItemId(R.id.navigation_short);
+
+		curtime = Calendar.getInstance();
 	}
 
 	private void makeShortScheduleView() {
-		contentView.removeAllViews(); // очищаем от добавленных ранее отображений
-		Date now = curtime.getTime(); // определяем текущее время
+		curtime = Calendar.getInstance();
+		STime now = new STime(curtime.getTime());
 
+		contentView.removeAllViews(); // очищаем от добавленных ранее отображений
 		if (shortView == null) {
+			Log.i("shortView", "inflating");
 			shortView = layoutInflater.inflate(R.layout.short_layout, null);
-			RadioGroup radioGroup = (RadioGroup) shortView.findViewById(R.id.radio_group);
-			radioGroup.setOnCheckedChangeListener(mOnRadioGroupChangedListener);
-			toTechnopolis = ((RadioButton) shortView.findViewById(R.id.rb_to)).isActivated();
+		}
+		contentView.addView(shortView);
+
+		if (timeTable == null) {
+			Log.i("timeTable", "loading");
+			timeTable = dataLoader.getFullDefaultInfo(); // объект с расписанием
 		}
 
 		TableLayout table = shortView.findViewById(R.id.table);
+
+		if (toggleButtonToTechnopolis == null) {
+			toggleButtonToTechnopolis = shortView.findViewById(R.id.toggle_to);
+			toggleButtonToTechnopolis.setOnClickListener(toggleToTechnopolis);
+		}
+
+		if (toggleButtonToUnderground == null) {
+			toggleButtonToUnderground = shortView.findViewById(R.id.toggle_from);
+			toggleButtonToUnderground.setOnClickListener(toggleToUnderground);
+		}
+
 		table.removeAllViews();
 		table.addView(layoutInflater.inflate(R.layout.short_head, null));
 
-		if (timeTable == null) timeTable = dataLoader.getFullDefaultInfo(); // объект с расписанием
 
-		if (curtime.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY) { // особое расписание по пятницам
-			CheckBox friday_view = shortView.findViewById(R.id.friday); // TODO: универсализировать для любых наборов дней недели
-			friday_view.setChecked(true);
-		}
+		Spinner weekdays = shortView.findViewById(R.id.spinner_weekdays);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.weekdays, android.R.layout.simple_spinner_item); // Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // Apply the adapter to the spinner
+		weekdays.setAdapter(adapter);
+		int we = curtime.get(Calendar.DAY_OF_WEEK) - Calendar.MONDAY;
+		weekdays.setSelection(we);
 
-		List<Date> closest = new ArrayList<>(3);
-		for (TimeTable.Line line : timeTable.lines) {
-			if ((toTechnopolis == line.to || toTechnopolis != line.from) && !line.isBefore(now))
-				closest.add(line.time);
-			if (closest.size() >= COUNT_TO_SHOW_ON_SHORT) break;
-		}
 
-		if (closest.size() == 0) {
+
+
+		ToggleButton from_tumbler = (ToggleButton) shortView.findViewById(R.id.toggle_from);
+		List<STime> after = timeTable.getTimeAfter(now, from_tumbler.isChecked());
+
+		for (int i = 0; i < Math.min(after.size(), COUNT_TO_SHOW_ON_SHORT); i++)
+			table.addView(getTimeLeftRow(after.get(i), now));
+
+		if (after.size() == 0) {
 			TableRow empty_row = new TableRow(getApplicationContext());
 			TextView empty_text = new TextView(getApplicationContext());
-			empty_text.setText("На сегодня автобусов в этом направлении больше нет.");
+			empty_text.setText("Cегодня автобусов в этом направлении больше нет.");
+			empty_text.setTextColor(Color.DKGRAY);
+			empty_text.setPadding(5, 5, 5, 5);
 			empty_row.addView(empty_text);
-			table.addView(empty_row);
-		} else
-			for (Date t : closest) {
-				table.addView(makeTimeLeftRow(t, now));
-			}
 
-		contentView.addView(shortView);
+			table.removeAllViews();
+			table.addView(empty_row);
+		}
 	}
+
+	public View getTimeLeftRow(DataLoader.STime t, STime now) {
+		STime left = now.getDifference(t);
+
+		String timeLeft = "";
+		if (left.isZero())
+			timeLeft = "прямо сейчас";
+		else {
+			if (left.hour != 0) timeLeft += " " + left.hour + " час";
+			if (left.min != 0) timeLeft += " " + left.min + " мин";
+		}
+
+		View row = layoutInflater.inflate(R.layout.short_row, null);
+		((TextView) row.findViewById(R.id.time)).setText(t.hour + ":" + (t.min <= 9 ? "0" : "") + t.min);
+		((TextView) row.findViewById(R.id.timeleft)).setText(timeLeft);
+
+		return row;
+	}
+
 
 	private void makeFullScheduleView() {
 		if (fullView == null) {
@@ -138,16 +182,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 	}
 
-	public void makeMapView() {
-		contentView.removeAllViews(); // очищаем от созданных ранее объектов
-		mapView = layoutInflater.inflate(R.layout.map_layout, null);
-
-		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-		mapFragment.getMapAsync(this);
-
-		contentView.addView(mapView);
-
-	}
 
 	public TableLayout makeThreeColumnsTable(TimeTable timeTable) {
 		TableLayout table = new TableLayout(this); //fullView.findViewById(R.id.table); // находим таблицу на созданном view
@@ -159,12 +193,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 	}
 
 	public TableLayout makeTwoColumnsTable(TimeTable timeTable) {
-		Date now = curtime.getTime();
+		STime now = new STime(Calendar.getInstance().getTime());
 		TableLayout table = new TableLayout(this);  // находим таблицу на созданном view
 
-		List<Date>
-				from = new ArrayList<Date>(),
-				to   = new ArrayList<Date>();
+		List<STime>
+				from = new ArrayList<DataLoader.STime>(),
+				to   = new ArrayList<DataLoader.STime>();
 
 		if (showPast) {
 			int max = timeTable.from.length;
@@ -175,12 +209,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 						(i < timeTable.to  .length ? timeTable.to  [i] : null))); // суём инфу в таблицу
 			}
 		} else {
-			for (Date d : timeTable.from)
-				if (d.getHours() > now.getHours() || (d.getHours() == now.getHours() && d.getMinutes() > now.getMinutes()))
+			for (STime d : timeTable.from)
+				if (d.hour > now.hour || (d.hour == now.hour && d.min > now.min))
 					from.add(d);
 
-			for (Date d : timeTable.to)
-				if (d.getHours() > now.getHours() || (d.getHours() == now.getHours() && d.getMinutes() > now.getMinutes()))
+			for (STime d : timeTable.to)
+				if (d.hour > now.hour || (d.hour == now.hour && d.min > now.min))
 					to.add(d);
 
 			for (int i = 0; i < Math.max(from.size(), to.size()); i++)
@@ -191,23 +225,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 		return table;
 	}
 
-	public View makeTwoColumnsRow(Date t1, Date t2) {
+	public View makeTwoColumnsRow(DataLoader.STime t1, DataLoader.STime t2) {
 		View row = layoutInflater.inflate(R.layout.full_2col_row, null);
-		Date now = Calendar.getInstance().getTime();
+		STime now = new DataLoader.STime(Calendar.getInstance().getTime());
 
 		TextView tFrom = (TextView) row.findViewById(R.id.t_from);
 		TextView tTo = (TextView) row.findViewById(R.id.t_to);
 
 		if (t1 == null) tFrom.setText("");
 		else {
-			tFrom.setText(t1.getHours() + ":" + (t1.getMinutes() <= 9 ? "0" : "") + t1.getMinutes()); //TODO: format
+			tFrom.setText(t1.hour + ":" + (t1.min <= 9 ? "0" : "") + t1.min); //TODO: format
 			if (firstIsBefore(t1, now))
 				tFrom.setTextColor(Color.LTGRAY);
 		}
 
 		if (t2 == null) tTo.setText("");
 		else {
-			tTo.setText(t2.getHours() + ":" + (t2.getMinutes() <= 9 ? "0" : "") + t2.getMinutes()); //TODO: format
+			tTo.setText(t2.hour + ":" + (t2.min <= 9 ? "0" : "") + t2.min); //TODO: format
 			if (firstIsBefore(t2, now))
 				tTo.setTextColor(Color.LTGRAY);
 		}
@@ -215,10 +249,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 		return row;
 	}
 
-	public boolean firstIsBefore(Date d1, Date d2) {
-		if (d1.getHours() < d2.getHours())
+	public boolean firstIsBefore(DataLoader.STime d1, DataLoader.STime d2) {
+		if (d1.hour < d2.hour)
 			return true;
-		if (d1.getHours() == d2.getHours() && d1.getMinutes() < d2.getMinutes())
+		if (d1.hour == d2.hour && d1.min < d2.min)
 			return true;
 		return false;
 	}
@@ -231,39 +265,29 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 		ImageView imFrom = (ImageView) row.findViewById(R.id.from);
 		ImageView imTo = (ImageView) row.findViewById(R.id.to);
 
-		text.setText(line.time.getHours() + ":" + (line.time.getMinutes() <= 9 ? "0" : "") + line.time.getMinutes()); //TODO: format
+		text.setText(line.time.hour + ":" + (line.time.min <= 9 ? "0" : "") + line.time.min); //TODO: format
 		imFrom.setVisibility(line.from ? View.VISIBLE : View.INVISIBLE);
 		imTo.setVisibility(line.to ? View.VISIBLE : View.INVISIBLE);
 
-		if (line.isBefore(calendar.getTime()))
+		if (line.isBefore(new STime(calendar.getTime())))
 			text.setTextColor(Color.LTGRAY);
 
 		return row;
 	}
 
-	public View makeTimeLeftRow(Date t, Date today) {
-		int leftH = t.getHours() - today.getHours();
-		int leftM = t.getMinutes() - today.getMinutes();
+	public void makeMapView() {
+		contentView.removeAllViews(); // очищаем от созданных ранее объектов
+		if (mapView == null) mapView = layoutInflater.inflate(R.layout.map_layout, null);
 
-		String timeLeft;
-		if (leftH == 0 && leftM == 0)
-			timeLeft = "(прямо сейчас)";
-		else {
-			if (leftM < 0) {
-				leftH--;
-				leftM += 60;
-			}
-			timeLeft = ""; //"(осталось";
-			if (leftH != 0) timeLeft += " " + leftH + " час";
-			if (leftM != 0) timeLeft += " " + leftM + " мин";
-			//timeLeft += ")";
-		}
+		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+		mapFragment.getMapAsync(this);
 
-		View row = layoutInflater.inflate(R.layout.short_row, null);
-		((TextView) row.findViewById(R.id.time)).setText(t.getHours() + ":" + (t.getMinutes() <= 9 ? "0" : "") + t.getMinutes());
-		((TextView) row.findViewById(R.id.timeleft)).setText(timeLeft);
+		contentView.addView(mapView);
+	}
 
-		return row;
+	public void setContent(View view) {
+		contentView.removeAllViews();
+		contentView.addView(view);
 	}
 
 
@@ -292,13 +316,22 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 		}
 	};
 
-	private RadioGroup.OnCheckedChangeListener mOnRadioGroupChangedListener
-			= new RadioGroup.OnCheckedChangeListener() {
+	private ToggleButton.OnClickListener toggleToTechnopolis
+			= new ToggleButton.OnClickListener() {
 		@Override
-		public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
-			Log.i("listener", "toTechnopolis changed");
-			Log.i("radiobutton", "нажата радиокнопка");
-			toTechnopolis = ((RadioButton) findViewById(R.id.rb_to)).isActivated();
+		public void onClick(View view) {
+			Log.i("listener", "toggle To clicked");
+			toggleButtonToUnderground.setChecked(!toggleButtonToTechnopolis.isChecked());
+			makeShortScheduleView();
+		}
+	};
+
+	private ToggleButton.OnClickListener toggleToUnderground
+			= new ToggleButton.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			Log.i("listener", "toggle From clicked");
+			toggleButtonToTechnopolis.setChecked(!toggleButtonToUnderground.isChecked());
 			makeShortScheduleView();
 		}
 	};
